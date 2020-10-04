722eafad00b0aa9df67477f8d355fc7fe3817bf5
Rakumairu <demaspiraa@gmail.com>
2020-10-04 18:59:07 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  // We've already handled the css purge in postcss.config.js
+  purge: false,
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
